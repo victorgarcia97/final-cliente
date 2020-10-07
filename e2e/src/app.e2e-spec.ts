42d69e76455fceea3a838d5c41d3d235fae72ec8
Victor Garcia <vgg1497@gmail.com>
2020-10-07 12:19:14 +0200

    Implementado test e2e en tipos de evento

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,17 +1,81 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('tipos-evento', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("debe navegar a la seccion de tipos de evento y estar los componentes",()=>{
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('curso16finalescliente app is running!');
-  });
+    page.getNavegacionTipoEventos().click();
+    
+
+    expect(page.getTituloTipoEventos().getText()).toEqual("Listado de tipos de evento");
+    expect(page.getBotonCrearTipoEvento().getText()).toEqual("Añadir tipo de evento");
+    expect(page.getBotonCrearTipoEvento()).toBeTruthy();
+  })
+
+  it("debe crear un nuevo tipo de evento", ()=>{
+    let competicion = "Dakar"
+    page.navigateTo();
+    page.getNavegacionTipoEventos().click();
+    page.getBotonCrearTipoEvento().click();
+    page.getSelectorDeporteTipoEvento().sendKeys("MotorSport");
+  
+    page.getInputCompeticion().sendKeys(competicion);
+    page.getInputFechaInicio().sendKeys("2020-10-07T00:00:00")
+      .then(response => page.getInputFechaFin().sendKeys("2020-10-29T00:00:00"));
+    page.pausar();
+    page.getBtnCrearGuardar().click();
+    page.getBtnVolver().click();
+    let textoCompeticion = page.getTiposEventoRows().get(-1).getText().then(response => response.split(" ")[1]);
+    page.pausar();
+    expect(textoCompeticion).toEqual(competicion);
+  })
+
+  it("debe modificar un tipo de evento", () => {
+    page.navigateTo();
+    page.getNavegacionTipoEventos().click();
+    let textoCompeticionModificado = page.getTiposEventoRows().get(-1).getText().then(response => response.split(" ")[1]);
+    page.getTiposEventoRows().get(-1).click();
+    page.pausar();
+    page.getInputCompeticion().clear();
+    page.getInputCompeticion().sendKeys("NBA");
+    page.getBtnCrearGuardar().click();
+    page.getBtnVolver().click();
+    let textoCompeticion = page.getTiposEventoRows().get(-1).getText().then(response => response.split(" ")[1]);
+    page.pausar();
+    expect(textoCompeticion).not.toBe(textoCompeticionModificado)
+    expect(textoCompeticion).toBe("NBA");
+
+  })
+
+  it("debe borrar un tipo de evento", () => {
+    page.navigateTo();
+    page.getNavegacionTipoEventos().click();
+    let numRowsBorrar;
+    
+    numRowsBorrar = page.getTiposEventoRows().count().then(num => num);
+
+    page.getTiposEventoRows().get(-1).click().then(()=>{
+      page.getBtnBorrar().click()
+        .then(() => page.getBtnConfirmarModal().click())
+    })
+    page.pausar();
+
+    let numRows;
+
+    numRows = page.getTiposEventoRows().count().then(num => num);
+
+    expect(numRows).not.toBe(numRowsBorrar);
+  })
+
+  
+
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,10 +2,73 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
+    browser.driver.manage().window().maximize();
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getNavegacionInicio(){
+    return element(by.id("navInicio"));
   }
+
+  getNavegacionTipoEventos(){
+    return element(by.id("navTipoEventos"));
+  }
+
+  getNavegacionTipoApuestas(){
+    return element(by.id("navTipoApuestas"));
+  }
+
+  getTituloTipoEventos(){
+    return element(by.id("tituloTipoEventos"));
+  }
+
+  getTiposEventoRows(){
+    return element.all(by.id("tipoEventoRow"));
+  }
+
+  getBotonCrearTipoEvento(){
+    return element(by.id("btnCrearTipoEvento"));
+  }
+
+  getSelectorDeporteTipoEvento(){
+    return element(by.id("selectorDeporte"));
+  }
+
+  getInputCompeticion(){
+    return element(by.id("inputCompeticion"));
+  }
+
+  getInputFechaInicio(){
+    return element(by.id("inputFechaInicio"));
+  }
+
+  getInputFechaFin(){
+    return element(by.id("inputFechaFin"));
+  }
+
+  getInputDescripcion(){
+    return element(by.id("inputDescripcion"));
+  }
+
+  getBtnVolver(){
+    return element(by.id("btnVolver"));
+  }
+
+  getBtnCrearGuardar(){
+    return element(by.id("btnCrearGuardar"));
+  }
+
+  getBtnBorrar(){
+    return element(by.id("btnBorrar"));
+  }
+
+  getBtnConfirmarModal(){
+    return element(by.id("btnConfirmarModal"));
+  }
+
+
+  pausar(){
+    browser.sleep(500);
+  }
+
 }
